1ce87fc421a9ed6a05f4cc4ea586af132af216c9
toma <tomajohn316@yahoo.com>
2022-07-01 13:56:56 +0100

    Refactor calculator with hooks

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,43 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Btn from './btn';
 import calculate from '../logic/calculate';
 
 const buttonArr = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+  const [solution, getSolution] = useState({ total: 0, next: null, operation: null });
 
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
+  const handleClick = (e) => {
     const Target = e.target.textContent;
-    this.setState((state) => calculate({
-      next: state.next,
-      total: state.total,
-      operation: state.operation,
-    }, Target));
-  }
+    getSolution(calculate(solution, Target));
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calc">
-        <input type="text" value={(total === null ? '' : total) + (operation === null ? '' : operation) + (next === null ? '' : next)} disabled className="calc-input" placeholder="0" />
-        <div className="btn-contain">
-          {buttonArr.map((btn) => (
-            <Btn key={btn} btnName={btn} event={this.handleClick} />
-          ))}
-        </div>
+  const { total, next, operation } = solution;
+  return (
+    <div className="calc">
+      <input type="text" value={(total === null ? '' : total) + (operation === null || operation === undefined ? '' : operation) + (next === null ? '' : next)} disabled className="calc-input" placeholder="0" />
+      <div className="btn-contain">
+        {buttonArr.map((btn) => (
+          <Btn key={btn} btnName={btn} event={handleClick} />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
